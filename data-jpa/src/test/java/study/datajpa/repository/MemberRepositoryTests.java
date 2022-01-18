3c97fe9e35e99fe929c4da72d5a059bd26d146a4
DESKTOP-FP38J33\minHong <coals0329@cocen.com>
2022-01-18 15:32:53 +0900

    [minHong] 메소드 이름으로 쿼리생성(레퍼런스 최고)

--- data-jpa/src/test/java/study/datajpa/repository/MemberRepositoryTests.java
+++ data-jpa/src/test/java/study/datajpa/repository/MemberRepositoryTests.java
@@ -10,13 +10,12 @@ import study.datajpa.entity.Member;
 
 @SpringBootTest
 @Rollback(false)
+@Transactional
 public class MemberRepositoryTests {
 
     @Autowired
     private MemberRepository memberRepository;
 
-    @Test
-    @Transactional
     public void findTest() {
         Member member = new Member("tester1");
         Member savedMember = memberRepository.save(member);
@@ -26,6 +25,24 @@ public class MemberRepositoryTests {
         System.out.println(Assertions.assertThat(findMember.getId()).isEqualTo(member.getId()));
         System.out.println(Assertions.assertThat(findMember.getUserName()).isEqualTo(member.getUserName()));
         System.out.println(Assertions.assertThat(findMember).isEqualTo(member));
+    }
+
+    public void findByUserNameAndAgeGreaterThan() {
+        Member member1 = new Member("tester1", 10);
+        Member member2 = new Member("tester2", 20);
+        memberRepository.save(member1);
+        memberRepository.save(member2);
+
+        System.out.println(memberRepository.findByUserNameAndAgeGreaterThan("tester1", 10));
+
+    }
+
+    @Test
+    public void finByAgeOrderByAgeDese() {
+        Member member1 = new Member("tester1", 10);
+        Member member2 = new Member("tester1", 20);
+
+        System.out.println(memberRepository.findByUserNameOrderByAgeDesc("tester1"));
     }
 
 }
